1c4c161d8846d46661d21be77bfa75e6dc6c020c
dwjclark11 <dwjclark1@hotmail.com>
2022-03-09 10:29:43 -0500

    C_Style AVL ->Added Delete amd other rotation functions

--- DataStructuresAndAlgorithms/C_Style/AVL.h
+++ DataStructuresAndAlgorithms/C_Style/AVL.h
@@ -111,11 +111,35 @@ struct Node* LRRotation(struct Node* p)
 
 struct Node* RRRotation(struct Node* p)
 {
+    /*
+        Before Rotation
+
+            [p]
+            (A)
+            /  \ [pr]
+           al   (B)
+               /  \
+        [prl] bl  (C)
+                  / \
+                 cl  cr
+
+        After Rotation
+
+               [pr] 
+               (B)
+              /   \
+      [p]-> (A)   (C)
+            / \   / \
+           al bl cl  cr
+               ^
+            [prl]
+    */
+
     struct Node* pr = p->rChild;
     struct Node* prl = pr->lChild;
 
     pr->lChild = p;
-    p->lChild = prl;
+    p->rChild = prl;
 
     p->height = NodeHeight(p);
     pr->height = NodeHeight(pr);
@@ -128,7 +152,46 @@ struct Node* RRRotation(struct Node* p)
 
 struct Node* RLRotation(struct Node* p)
 {
-    return NULL;
+
+    /*
+        Before Rotation
+
+              [p]
+              (A)
+              / \
+             al (B) [pr]
+                / \
+        [prl] (C)  bl
+              / \
+             cl  cr
+
+        After Rotation
+             [prl]
+              (C)
+             /   \
+       [p] (A)   (B) [pr]
+           / \   / \
+          al cl  cr br 
+    */
+
+    struct Node* pr = p->rChild;
+    struct Node* prl = pr->lChild;
+
+    p->rChild = prl->lChild;
+    pr->lChild = prl->rChild;
+
+    prl->rChild = pr;
+    prl->lChild = p;
+
+    // Update the heights
+    p->height = NodeHeight(p);
+    pr->height = NodeHeight(pr);
+    prl->height = NodeHeight(prl);
+
+    if (root == p)
+        root = prl;
+
+    return prl;
 }
 
 struct Node* RInsert(struct Node** p, int key)
@@ -144,11 +207,13 @@ struct Node* RInsert(struct Node** p, int key)
         return t;
     }
 
+    // Check the key to the current data and call recursively the correct child node
     if (key < (*p)->data)
         (*p)->lChild = RInsert(&(*p)->lChild, key);
     else if (key > (*p)->data)
         (*p)->rChild = RInsert(&(*p)->rChild, key);
 
+    // Update the height of p
     (*p)->height = NodeHeight(*p);
 
     // Check the balance factors to see what type of rotation may be needed
@@ -161,6 +226,113 @@ struct Node* RInsert(struct Node** p, int key)
     else if (BalanceFactor(*p) == -2 && BalanceFactor((*p)->rChild) == 1)
         return RLRotation(*p);
 
+    return *p;
+}
+
+
+struct Node* search(struct Node* root, int key)
+{
+    struct Node* t = root;
+
+    while (t != NULL)
+    {
+        if (key == t->data)
+            return t;
+        else if (key < t->data)
+            t = t->lChild;
+        else
+            t = t->rChild;
+    }
+
+    return NULL;
+}
+
+
+int Height(struct Node* p)
+{
+    int x, y;
+    if (p == NULL)
+        return 0;
+
+    x = Height(p->lChild);
+    y = Height(p->rChild);
+
+    return x > y ? x + 1 : y + 1;
+}
+
+// Return the InOrder Predecessor of the current Node
+struct Node* InPre(struct Node* p)
+{
+    while (p && p->rChild)
+        p = p->rChild;
+
+    return p;
+}
+
+// Return the InOrder Successor of the current Node
+struct Node* InSucc(struct Node* p)
+{
+    while (p && p->lChild)
+        p = p->lChild;
+
+    return p;
+}
+
+struct Node* Delete(struct Node** p, int key)
+{
+    
+    if ((*p) == NULL)
+        return NULL;
+
+    if ((*p)->lChild == NULL && (*p)->rChild == NULL)
+    {
+        // If p is the main root, set the root to NULL
+        if (root == (*p))
+            root = NULL;
+
+        free(*p);
+        return NULL;
+    }
+    
+    // Compare key to data to see what child node needs to be called recursively
+    if (key < (*p)->data)
+        (*p)->lChild = Delete(&(*p)->lChild, key);
+    else if (key > (*p)->data)
+        (*p)->rChild = Delete(&(*p)->rChild, key);
+    else
+    {
+        struct Node* q = NULL;
+
+        if (Height((*p)->lChild) > Height((*p)->rChild))
+        {
+            q = InPre((*p)->lChild);
+            (*p)->data = q->data;
+            (*p)->lChild = Delete(&(*p)->lChild, q->data);
+        }
+        else
+        {
+            q = InSucc((*p)->rChild);
+            (*p)->data = q->data;
+            (*p)->rChild = Delete(&(*p)->rChild, q->data);
+        }
+    }
+
+    // Update the height of p
+    (*p)->height = NodeHeight(*p);
+
+    // Check the balance factors to see what type of rotation may be needed
+    if (BalanceFactor(*p) == 2 && BalanceFactor((*p)->lChild) == 1)
+        return LLRotation((*p));
+    else if (BalanceFactor(*p) == 2 && BalanceFactor((*p)->lChild) == -1)
+        return LRRotation(*p);
+    else if (BalanceFactor(*p) == 2 && BalanceFactor((*p)->lChild) == 0)
+        return LLRotation((*p));
+    else if (BalanceFactor(*p) == -2 && BalanceFactor((*p)->rChild) == -1)
+        return RRRotation(*p);
+    else if (BalanceFactor(*p) == -2 && BalanceFactor((*p)->rChild) == 1)
+        return RLRotation(*p);
+    else if (BalanceFactor(*p) == -2 && BalanceFactor((*p)->rChild) == 0)
+        return RRRotation(*p);
 
     return *p;
 }
--- DataStructuresAndAlgorithms/main.cpp
+++ DataStructuresAndAlgorithms/main.cpp
@@ -3,9 +3,13 @@
 
 int main()
 {
+    RInsert(&root, 50);
     RInsert(&root, 10);
-    RInsert(&root, 5);
-    RInsert(&root, 2);
+    RInsert(&root, 20);
+    RInsert(&root, 25);
+    RInsert(&root, 15);
+    Delete(&root, 15);
+
 
 	return 0;
 } 
